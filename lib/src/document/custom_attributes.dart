1abf33d03537d6b215f9ea94e748626770b50535
chenjincan <865525900@qq.com>
2025-12-12 01:10:18 +0800

    feat: 增加属性

--- lib/src/document/custom_attributes.dart
+++ lib/src/document/custom_attributes.dart
@@ -23,3 +23,25 @@ class LineHeightAttribute extends Attribute<double?> {
   static const Attribute<double?> lineHeightDouble =
       LineHeightAttribute(lineHeight: 2);
 }
+
+/// This attribute represents the reminder time for a task/checkbox item.
+/// The value is stored as milliseconds since epoch (DateTime.millisecondsSinceEpoch).
+///
+/// The attribute at the json looks like: "attributes":{"task-reminder": 1234567890123 }
+class TaskReminderAttribute extends Attribute<int?> {
+  const TaskReminderAttribute({int? reminderTime})
+      : super('task-reminder', AttributeScope.block, reminderTime);
+}
+
+/// This attribute represents the priority level for a task/checkbox item.
+/// Priority values: 1 = low, 2 = medium, 3 = high
+///
+/// The attribute at the json looks like: "attributes":{"task-priority": 2 }
+class TaskPriorityAttribute extends Attribute<int?> {
+  const TaskPriorityAttribute({int? priority})
+      : super('task-priority', AttributeScope.block, priority);
+
+  static const Attribute<int?> low = TaskPriorityAttribute(priority: 1);
+  static const Attribute<int?> medium = TaskPriorityAttribute(priority: 2);
+  static const Attribute<int?> high = TaskPriorityAttribute(priority: 3);
+}
